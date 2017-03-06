2858c6f6a7814e9a5bf5cc4cbc300e1aad19029d
kevinfan23 <kevinfan23@ucla.edu>
2017-03-05 18:33:26 -0800

    added grid class methods

--- lab5/grid/__init__.py
+++ lab5/grid/__init__.py
@@ -1,15 +1,28 @@
 
 import numpy as np
 
+OBSTACLE = -1
+DISCOVERED = 1
+CURRENT = 9
+
 class Grid(object):
 
     def __init__(self, n, m):
         # Creates a matrix.
         self.grid = np.zeros((n,m))
 
-    def add_obstacle(self, row, column, value):
-        self.grid[row - 1][column - 1] = value
+    def add_obstacle(self, row, column):
+        self.grid[row - 1][column - 1] = OBSTACLE
         return
+
+    def mark_discovered(self. row, column)
+        self.grid[row - 1][column - 1] = DISCOVERED
+        return
+
+    def set_current(self. row, column):
+        self.grid[row - 1][column - 1] = CURRENT
+        return
+
 
     def print_grid():
         return self.grid
